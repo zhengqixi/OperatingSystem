93d6d9e87820e6d5bf76e0c71be5c87e5418180e
zhengqixi <zhengqixi@github.com>
2016-10-19 23:16:06 -0400

    Basic functionality done

--- Problem 3/mybash.c
+++ Problem 3/mybash.c
@@ -4,6 +4,7 @@
 #include<stdio.h>
 #include<errno.h>
 #include<sys/types.h>
+#include<sys/wait.h>
 
 #define ARG_MAX 4096
 
@@ -15,53 +16,45 @@ void errorReport(char * error_message){
 
 void runCommand(char ** command){
     pid_t process = fork(); 
+    if (process == 0){
+        int status = execvp(command[0], command); 
+        if (status < 0){
+            errorReport("Error executing command\n");
+        }
+    } else {
+        int return_status;
+        waitpid(process, &return_status, 0);
+    }
     
 }
 
-char ** getCommand(FILE * stream){
-    char command_line[ARG_MAX];
-    int i = 0;
-    char current;
-    while ( (current = fgetc(stream)) != '\n'){
-        command_line[i] = current;
-        ++i;
-    }
-    command_line[i] = '\0';
-
-    int arguments = 1;
-    char ** command = (char **)malloc(sizeof(char *) * arguments+1);
-    char * argument = strtok(command_line, " ");
-    command[0] = (char *)malloc(sizeof(char) * ARG_MAX);
-    strcpy(command[0], argument);
-
-    while ( (argument = strtok(NULL, " ")) != NULL){
-        ++arguments;
-        command = (char **)realloc(command, sizeof(char *) * arguments + 1);
-        command[arguments-1] = (char *)malloc(sizeof(char) * ARG_MAX);
-        strcpy(command[arguments-1], argument);
-    }
-
-    command[arguments] = NULL;
-    return command;
-
-}
-
-void freeCommand(char ** command){
-    char ** state = command;
-    while ((*command) != NULL){
-        printf("%s\n", (*command));
-        free((*command));
-        ++command;
-    }
-    free(state);
-}
 
 int main(int argc, char* argv[]) {
+    char * command[ARG_MAX];
+    FILE * stream = stdin;
     if (argc  > 1){
+        stream = fopen(argv[1], "r");
     }
     while(1){
-        char ** command = getCommand(stdin);
+        char command_line[ARG_MAX];
+        int i = 0;
+        char current;
+        while ( (current = fgetc(stream)) != '\n'){
+            command_line[i] = current;
+            ++i;
+        }
+        command_line[i] = '\0';
+        int arguments = 1;
+        char * argument = strtok(command_line, " ");
+        command[0] = argument;
+
+        while ( (argument = strtok(NULL, " ")) != NULL){
+            ++arguments;
+            command[arguments-1] = argument; 
+        }
+
+        command[arguments] = NULL;
+
         runCommand(command);
-        freeCommand(command);
     }
 }
