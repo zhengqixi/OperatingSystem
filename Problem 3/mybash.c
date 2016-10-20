9101094e4fbd54800ebe72b1056b5ce53df4a02b
zhengqixi <zhengqixi@github.com>
2016-10-20 00:03:12 -0400

    Can now read from scripts

--- Problem 3/mybash.c
+++ Problem 3/mybash.c
@@ -5,6 +5,7 @@
 #include<errno.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+#include<time.h>
 
 #define ARG_MAX 4096
 
@@ -19,11 +20,12 @@ void runCommand(char ** command){
     if (process == 0){
         int status = execvp(command[0], command); 
         if (status < 0){
-            errorReport("Error executing command\n");
+            errorReport("Error executing command");
         }
     } else {
         int return_status;
         waitpid(process, &return_status, 0);
+        printf("Exit status of process: %d\n", WEXITSTATUS(return_status));
     }
     
 }
@@ -34,16 +36,25 @@ int main(int argc, char* argv[]) {
     FILE * stream = stdin;
     if (argc  > 1){
         stream = fopen(argv[1], "r");
+        if (stream == NULL){
+            errorReport("Cannot open input file");
+        }
     }
     while(1){
         char command_line[ARG_MAX];
         int i = 0;
         char current;
         while ( (current = fgetc(stream)) != '\n'){
+            if (feof(stream)){
+                return 0;
+            }
             command_line[i] = current;
             ++i;
         }
         command_line[i] = '\0';
+        if (command_line[0] == '#'){
+            continue;
+        }
         int arguments = 1;
         char * argument = strtok(command_line, " ");
         command[0] = argument;
@@ -55,6 +66,13 @@ int main(int argc, char* argv[]) {
 
         command[arguments] = NULL;
 
+        time_t start = time(NULL);
+
         runCommand(command);
+
+        time_t end = time(NULL);
+
+
+
     }
 }
